b20944f5f3d0ca145935041cdbabd1af89818731
VladislavYglik <wtbfreenickname@gmail.com>
2022-12-16 17:57:00 +0300

    update
    added default filter name into SearchFilter repository

--- app/src/main/java/com/example/foodinfo/repository/RepositorySearchFilter.kt
+++ app/src/main/java/com/example/foodinfo/repository/RepositorySearchFilter.kt
@@ -14,21 +14,40 @@ interface RepositorySearchFilter {
     fun getQueryByLabel(categoryName: String, labelName: String): String
 
 
-    fun getCategory(filterName: String, categoryName: String): CategoryFilterEditModel
+    fun getCategory(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME, categoryName: String
+    ): CategoryFilterEditModel
 
-    fun getCategoriesPreview(filterName: String): List<CategoryFilterPreviewModel>
+    fun getCategoriesPreview(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME
+    ): List<CategoryFilterPreviewModel>
 
-    fun updateCategory(filterName: String, categoryName: String, category: CategoryFilterEditModel)
+    fun updateCategory(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME,
+        categoryName: String,
+        category: CategoryFilterEditModel
+    )
 
 
-    fun getNutrientsEdit(filterName: String): List<NutrientFilterEditModel>
+    fun getNutrientsEdit(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME
+    ): List<NutrientFilterEditModel>
 
-    fun getNutrientsPreview(filterName: String): List<NutrientFilterPreviewModel>
+    fun getNutrientsPreview(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME
+    ): List<NutrientFilterPreviewModel>
 
-    fun updateNutrients(filterName: String, nutrients: List<NutrientFilterEditModel>)
+    fun updateNutrients(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME,
+        nutrients: List<NutrientFilterEditModel>
+    )
 
 
-    fun getBaseFields(filterName: String): List<BaseFieldFilterEditModel>
+    fun getBaseFields(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME
+    ): List<BaseFieldFilterEditModel>
 
-    fun updateBaseFields(filterName: String, fields: List<BaseFieldFilterEditModel>)
+    fun updateBaseFields(
+        filterName: String = SearchFilterEntity.DEFAULT_NAME, fields: List<BaseFieldFilterEditModel>
+    )
 }
